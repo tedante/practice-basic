84dade6e95fdf39bb043bc676a963915aa0be702
Teddy <teddykoerniadi@gmail.com>
2024-02-26 21:28:52 +0700

    add

---
+++ 1/remove-character-from-string.js
@@ -0,0 +1,21 @@
+/**
+ * Summary
+ * Remove word after `#`
+ * 
+ * Input: 
+ * text {STRING}
+ * 
+ * Output:
+ * result {STRING}
+ * 
+ * Example
+ * Case 1
+ * Input: www.codewars.com#about
+ * Output: www.codewars.com
+ * 
+ * Case 2
+ * Input: www.codewars.com?page=1
+ * Output: www.codewars.com?page=1
+ * 
+ * Source: https://www.codewars.com/kata/51f2b4448cadf20ed0000386
+ */
